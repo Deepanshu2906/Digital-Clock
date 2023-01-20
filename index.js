2430954f8c8c572cc077789926a349a284e457b0
Deepanshu2906 <deepanshugoyal2906@gmail.com>
2023-01-20 17:46:51 +0530

    am/ pm test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function updateClock(){
      h= h<10 ? "0"+h : h;
      m = m<10 ? "0"+m : m;
      s = s<10 ? "0"+s : s;
-    // console.log( h,m,s);
+    console.log( h,m,s,ampm);
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
