fb243289df6ce2f3a45d084885c20817835c2da2
Deepanshu2906 <deepanshugoyal2906@gmail.com>
2023-01-20 13:30:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+
+function updateClock(){
+
+    let h= new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    let ampm = "AM";
+    if( h> 12){
+         ampm = "PM";
+         h = h-12 ;
+    }
+     h= h<10 ? "0"+h : h;
+     m = m<10 ? "0"+m : m;
+     s = s<10 ? "0"+s : s;
+    // console.log( h,m,s);
+    hourEl.innerText = h;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+     setTimeout(() => {
+        updateClock();
+     },1000);
+}
+updateClock();
